35c0f925945feabbc9c0137ebb561ccb2b8910b8
Morteza Malvandi <malvandi.m@gmail.com>
2025-01-04 09:39:02 +0330

    Add navigation

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,11 @@
 CREATE USER kyc_user WITH ENCRYPTED PASSWORD 'keycloak_user';
+CREATE USER nav_user WITH ENCRYPTED PASSWORD 'navigation_user';
 
 -- \c kyc_user
 CREATE DATABASE keycloak WITH OWNER kyc_user;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO kyc_user;
+
+-- \c nav_user
+CREATE DATABASE navigation WITH OWNER nav_user;
+GRANT ALL PRIVILEGES ON DATABASE navigation TO nav_user;
+-- todo
