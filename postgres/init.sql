e1a40346e7b31fdecce362db95b0ce69080c7d6e
Morteza Malvandi <malvandi.m@gmail.com>
2025-06-06 07:59:14 +0330

    Add n8n

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,11 +2,13 @@ CREATE USER kyc_user WITH ENCRYPTED PASSWORD 'keycloak_user';
 CREATE USER vs_user WITH ENCRYPTED PASSWORD 'vector_service_user';
 CREATE USER ts_user WITH ENCRYPTED PASSWORD 'tile_service_user';
 CREATE USER trader_service_user WITH ENCRYPTED PASSWORD 'trader_service_user';
+CREATE USER n8n_user WITH ENCRYPTED PASSWORD 'n8n-p@ssw0rd';
 
 CREATE DATABASE keycloak WITH OWNER kyc_user;
 CREATE DATABASE vector_service WITH OWNER vs_user;
 CREATE DATABASE tile_service WITH OWNER ts_user;
 CREATE DATABASE trader_service WITH OWNER trader_service_user;
+CREATE DATABASE n8n_service WITH OWNER n8n_user;
 
 \c kyc_user
 ALTER SCHEMA "public" OWNER TO "kyc_user";
@@ -23,3 +25,5 @@ ALTER SCHEMA "public" OWNER TO "ts_user";
 \c trader_service
 ALTER SCHEMA "public" OWNER TO "trader_service_user";
 
+\c n8n_user
+ALTER SCHEMA "public" OWNER TO "n8n_user";
